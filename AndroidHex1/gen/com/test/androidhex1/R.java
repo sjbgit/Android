4adc21aa057bae9325bb47ce6904c208a9dc7afa
sjbgit <sjb_strat@hotmail.com>
2014-06-02 23:33:37 -0500

    work on hex

--- AndroidHex1/gen/com/test/androidhex1/R.java
+++ AndroidHex1/gen/com/test/androidhex1/R.java
@@ -24,12 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int bottom=0x7f080003;
         public static final int left=0x7f080001;
         public static final int right=0x7f080002;
+        public static final int seekbarRed=0x7f080004;
         public static final int top=0x7f080000;
-        public static final int values=0x7f080004;
+        public static final int values=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
