ee22db0a61b768a405841708db8fe125a58b4657
sjbgit <sjb_strat@hotmail.com>
2014-11-17 14:47:25 -0600

    test app

--- TestRetro1/app/src/main/java/com/example/sbunke/testretro1/MyActivity.java
+++ TestRetro1/app/src/main/java/com/example/sbunke/testretro1/MyActivity.java
@@ -1,5 +1,9 @@
 package com.example.sbunke.testretro1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Locale;
 
 import android.app.Activity;
@@ -7,10 +11,13 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -18,6 +25,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
@@ -43,11 +56,81 @@ public class MyActivity extends Activity {
      */
     ViewPager mViewPager;
 
+    public static String GET(String url) {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String returnedValue = GET(urls[0]);
+            return returnedValue;
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            //etResponse.setText(result);
+        }
+    }
+
+    // convert inputstream to String
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    // check network connection
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
+        new HttpAsyncTask().execute("http://192.168.0.1:3333");
+
+
+        //GET("http://192.168.0.1:3333");
 
 
         // Create the adapter that will return a fragment for each of the three
@@ -58,7 +141,10 @@ public class MyActivity extends Activity {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        final RestApi restApi = new RestAdapter.Builder().setEndpoint("192.168.0.10").build().create(RestApi.class);
+
+
+        /*
+        final RestApi restApi = new RestAdapter.Builder().setEndpoint("192.168.0.1").build().create(RestApi.class);
 
         new AsyncTask<Void, Void, User>() {
 
@@ -80,6 +166,7 @@ public class MyActivity extends Activity {
                 RetrofitError error1 = error;
             }
         });
+        */
     }
 
 
